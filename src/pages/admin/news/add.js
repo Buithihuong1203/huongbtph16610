8e365399e3fc0dca97531346bda071bebada33a7
huongbtph16610 <buithihuong20080@gmail.com>
2022-02-07 15:12:46 +0700

    cách upload ảnh

--- src/pages/admin/news/add.js
+++ src/pages/admin/news/add.js
@@ -37,8 +37,11 @@ const AdminNewsAdd = {
                 <div class="px-4 py-6 sm:px-0">
                     <form id="form-add-post">
                         <input type="text" class="border border-black" id="title-post" placeholder="Title"/><br />
-                        <input type="file" class="border border-black" id="img-post" placeholder="Img" /><br />
+
+                        <input type="file" class="border border-black" id="img-post" " /><br />
+
                         <textarea name="" cols="30" rows="10" class="border border-black" id="desc-post" placeholder="Description"></textarea><br />
+
                         <button class="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Thêm</button>
                     </form>
                 </div>
@@ -51,6 +54,28 @@ const AdminNewsAdd = {
     afterRender() {
         // console.log(document.querySelector('#form-add-post'));
         const formAdd = document.querySelector('#form-add-post');
+        const imgPost = document.querySelector("#img-post");
+        imgPost.addEventListener("change", (e) => {
+            const file = e.target.files[0];
+            console.log(file);
+
+            // lấy giá trị của file upload cho sử dụng formData
+            const formData = new formData();
+            formData.append("file", file);
+            formData.append("upload_preset", "jkbdphzy");
+            //call api
+            axios({
+                url: "https://cloudinary.com/documentation/image_upload_api_reference",
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-formendcoded",
+
+                },
+                data: formData,
+
+            });
+        });
+
         formAdd.addEventListener('submit', function (e) {
             e.preventDefault();
             add({
